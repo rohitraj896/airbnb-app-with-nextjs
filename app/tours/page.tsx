2dd90e0138281336347b7a644a4de57096fd35eb
Rohit Kumar <rohitkr3390@gmail.com>
2024-07-31 16:18:26 +0530

    images config

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import Image from "next/image";
 const url = "https://www.course-api.com/react-tours-project";
 
 type Tour = {
@@ -22,17 +22,28 @@ async function ToursPage() {
   return (
     <section>
       <h1 className="text-3xl mb-4">Tours</h1>
-      {data.map(tour => {
-        return (
-          <Link
-            key={tour.id}
-            href={`/tours/${tour.id}`}
-            className="hover:text-blue-500"
-          >
-            <h2>{tour.name}</h2>
-          </Link>
-        );
-      })}
+      <div className="grid md:grid-cols-2 gap-8">
+        {data.map(tour => {
+          return (
+            <Link
+              key={tour.id}
+              href={`/tours/${tour.id}`}
+              className="hover:text-blue-500"
+            >
+              <div>
+                <Image
+                  src={tour.image}
+                  alt={tour.name}
+                  fill
+                  priority
+                  className="object-cover"
+                />
+              </div>
+              <h2>{tour.name}</h2>
+            </Link>
+          );
+        })}
+      </div>
     </section>
   );
 }
